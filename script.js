2710d4a436af441410d257b790c0d949a63d5447
Rhulani Chauke <65870358+General-Rhully@users.noreply.github.com>
2022-07-22 15:18:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+
+const wrapper = document.querySelector(".wrapper"),
+notiFi = wrapper.querySelector(".notiFi"),
+title = notiFi.querySelector("span"),
+subTitle = notiFi.querySelector("p"),
+wifiIcon = notiFi.querySelector(".icon"),
+closeIcon = notiFi.querySelector(".close-icon");
+
+window.onload = ()=>{
+    function ajax(){
+        let xhr = new XMLHttpRequest(); //creating new XML object
+        xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true); //sending get request on this URL
+        xhr.onload = ()=>{ //once ajax loaded
+            //if ajax status is equal to 200 or less than 300 that mean user is getting data from that provided url
+            //or his/her response status is 200 that means he/she is online
+            if(xhr.status == 200 && xhr.status < 300){
+                notiFi.classList.remove("offline");
+                title.innerText = "You're online now";
+                subTitle.innerText = "Internet is connected.";
+                wifiIcon.innerHTML = '<i class="uil uil-wifi"></i>';
+                closeIcon.onclick = ()=>{ 
+                    wrapper.classList.add("hide");
+                }
+                setTimeout(()=>{ //hide the notiFi notification automatically after 5 seconds
+                    wrapper.classList.add("hide");
+                }, 650);
+            }else{
+                offline(); //calling offline function if ajax status is not equal to 200 or not less that 300
+            }
+        }
+        xhr.onerror = ()=>{
+            offline(); ////calling offline function if the passed url is not correct or returning 404 or other error
+        }
+        xhr.send(); //sending get request to the passed url
+    }
+
+    function offline(){ //function for offline
+        wrapper.classList.remove("hide");
+        notiFi.classList.add("offline");
+        title.innerText = "You're offline now";
+        subTitle.innerText = "Oh no! Internet is disconnected.";
+        wifiIcon.innerHTML = '<i class="uil uil-wifi-slash"></i>';
+       
+    }
+
+    setInterval(()=>{ //this setInterval function call ajax frequently after 100ms
+        ajax();
+    }, 100);
+}
